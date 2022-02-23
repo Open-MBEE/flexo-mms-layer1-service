a6d47495c86021b7f8ecbf9ba3d0d8834d3e7aed
Jason Han <jason.han@jpl.nasa.gov>
2022-02-22 18:29:39 -0800

    Actually add the code

---
+++ mms/src/main/kotlin/org/openmbee/mms5/plugins/Authentication.kt
@@ -0,0 +1,27 @@
+package org.openmbee.mms5.plugins
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import io.ktor.application.*
+import io.ktor.auth.*
+import io.ktor.auth.jwt.*
+
+fun Application.configureAuthentication() {
+    authentication {
+        jwt {
+            val jwtAudience = environment.config.property("jwt.audience").getString()
+            val issuer = environment.config.property("jwt.domain").getString()
+            val secret = environment.config.property("jwt.secret").getString()
+            realm = environment.config.property("jwt.realm").getString()
+            verifier(
+                JWT.require(Algorithm.HMAC256(secret))
+                    .withAudience(jwtAudience)
+                    .withIssuer(issuer)
+                    .build()
+            )
+            validate { credential ->
+                if (credential.payload.audience.contains(jwtAudience)) JWTPrincipal(credential.payload) else null
+            }
+        }
+    }
+}
