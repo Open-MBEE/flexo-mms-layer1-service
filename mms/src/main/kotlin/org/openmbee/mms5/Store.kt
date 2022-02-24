e6ef23340f437c620b7b04e53a4a6c0945b4be64
Blake Regalia <blake.d.regalia@jpl.nasa.gov>
2022-02-23 16:22:00 -0800

    dev: ldap groups

--- mms/src/main/kotlin/org/openmbee/mms5/Store.kt
+++ mms/src/main/kotlin/org/openmbee/mms5/Store.kt
@@ -25,10 +25,10 @@ suspend fun handleSparqlResponse(response: HttpResponse): String {
 
 class IllegalIdException: Exception("Illegal ID string. Must be at least 3 characters long. Letter symbols and special characters '.' '-' '_' allowed.") {}
 
-private val LEGAL_ID_REGEX = """[._\pL-]{3,}""".toRegex()
+private val LEGAL_ID_REGEX = """[._\pL-]{3,256}""".toRegex()
 
-fun assertLegalId(id: String) {
-    if(!id.matches(LEGAL_ID_REGEX) || id.startsWith("__") || id.length > 256) {
+fun assertLegalId(id: String, regex: Regex=LEGAL_ID_REGEX) {
+    if(!id.matches(regex) || id.startsWith("__")) {
         throw IllegalIdException()
     }
 }
