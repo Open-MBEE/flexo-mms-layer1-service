18893c79c3443554e92b8a2498938dcfc5c71f28
Jason Han <jason.han@jpl.nasa.gov>
2022-03-17 12:05:11 -0700

    Remove ConsulPlugin

--- mms/src/main/kotlin/org/openmbee/mms5/Application.kt
+++ mms/src/main/kotlin/org/openmbee/mms5/Application.kt
@@ -4,7 +4,6 @@ import io.ktor.application.*
 import org.openmbee.mms5.plugins.configureAuthentication
 import org.openmbee.mms5.plugins.configureHTTP
 import org.openmbee.mms5.plugins.configureRouting
-import org.openmbee.mms5.plugins.registerService
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -12,10 +11,6 @@ fun Application.module(testing: Boolean=false) {
     configureAuthentication()
     configureHTTP()
     configureRouting()
-
-    if(environment.config.propertyOrNull("consul.enabled")?.getString() == "true") {
-        registerService()
-    }
 }
 
 
