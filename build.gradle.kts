94aaaead9759638b01eb761efffe4ee478338a27
Blake Regalia <blake.d.regalia@jpl.nasa.gov>
2022-06-22 19:04:34 -0700

    dev: kotest assertions

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,12 +18,22 @@ repositories {
     mavenCentral()
 }
 
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+}
+
 val testFuseki: Configuration by configurations.creating
 
 dependencies {
     implementation(kotlin("stdlib"))
 
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+    val kotestVersion = "5.3.1"
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest:kotest-property:$kotestVersion")
+
+    val kotestAssertionsKtorVersion = "1.0.3"
+    testImplementation("io.kotest.extensions:kotest-assertions-ktor:$kotestAssertionsKtorVersion")
 
     val commonsCliVersion = "1.5.0"
     implementation("commons-cli:commons-cli:$commonsCliVersion")
--- src/test/kotlin/org/openmbee/mms5/RepoTests.kt
+++ src/test/kotlin/org/openmbee/mms5/RepoTests.kt
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
 import org.openmbee.mms5.util.AuthObject
 import org.openmbee.mms5.util.TestBase
+import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
@@ -15,32 +16,43 @@ class RepoTests : TestBase() {
         username = "root",
         groups = listOf("super_admins")
     )
-    private val testOrgId = "testCreateAndReadOrg"
-    private val testOrgName = "OpenMBEE"
+    private val testOrgId = "repoTests"
+    private val testOrgName = "Repo Tests"
 
 
     @Test
-    @Order(2)
-    fun createOnValidOrg() {
+    @Order(1)
+    fun createOnNewOrg() {
         doCreateOrg(defaultAuthObject, testOrgId, testOrgName)
-
-        Thread.sleep(20000)
 
         withTestEnvironment {
             val put = handleRequest(HttpMethod.Put, "/orgs/$testOrgId/repos/new-repo") {
                 addAuthorizationHeader(defaultAuthObject)
                 setTurtleBody("""
                     <>
-                        dct:title "TMT"@en ;
-                        <https://demo.org/custom/prop> "2" ;
+                        dct:title "$testOrgName"@en ;
+                        <https://demo.org/custom/prop> "test" ;
                         .
                 """.trimIndent())
             }
             println("createOnValidOrg headers: " + put.response.headers.allValues().toString())
             println("createOnValidOrg status: " + put.response.status())
             println("createOnValidOrg content: " + put.response.content)
-            assertTrue(put.response.status()?.isSuccess() ?: true, "Create project on valid org")
+            assertTrue(put.response.status()?.isSuccess() ?: true, "Create repo on valid org")
         }
     }
 
+    @Test
+    @Order(2)
+    fun invalidRepoId() {
+        withTestEnvironment {
+            val put = handleRequest(HttpMethod.Put, "/orgs/$testOrgId/repos/invalid%20id") {
+                addAuthorizationHeader(defaultAuthObject)
+                setTurtleBody("""<> dct:title "$testOrgName"@en .""")
+            }
+
+            assertEquals("400 HTTP Status", 400, put.response.status())
+                put.response.status()?.isSuccess() ?: false, "Create repo with invalid id")
+        }
+    }
 }
