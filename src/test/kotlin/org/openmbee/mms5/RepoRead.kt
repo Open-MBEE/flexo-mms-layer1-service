3ecb8732825e6456217c6a4fcd38acde41702165
Jason Han <jason.han@jpl.nasa.gov>
2022-07-05 16:38:17 -0700

    WIP: Adding repo tests

--- src/test/kotlin/org/openmbee/mms5/RepoRead.kt
+++ src/test/kotlin/org/openmbee/mms5/RepoRead.kt
@@ -1,5 +1,6 @@
 package org.openmbee.mms5
 
+import io.kotest.assertions.ktor.shouldHaveHeader
 import io.kotest.assertions.ktor.shouldHaveStatus
 import io.kotest.core.test.TestCase
 import io.kotest.matchers.string.shouldNotBeBlank
@@ -11,6 +12,46 @@ import org.slf4j.LoggerFactory
 
 class RepoRead : RepoAny() {
     init {
-        //  tests...
+        "head non-existent repo" {
+            withTest {
+                httpHead(repoPath) {}.apply {
+                    response shouldHaveStatus HttpStatusCode.NotFound
+                }
+            }
+        }
+
+        "get non-existent repo" {
+            withTest {
+                httpGet(repoPath) {}.apply {
+                    response shouldHaveStatus HttpStatusCode.NotFound
+                }
+            }
+        }
+
+        "head repo" {
+            val create = createRepo(repoId, repoName, orgId)
+
+            withTest {
+                httpHead(repoPath) {}.apply {
+                    response shouldHaveStatus HttpStatusCode.OK
+                    response.shouldHaveHeader(HttpHeaders.ETag, create.response.headers[HttpHeaders.ETag]!!)
+                }
+            }
+        }
+
+        "get repo" {
+            val create = createRepo(repoId, repoName, orgId)
+
+            withTest {
+                httpGet(repoPath) {}.apply {
+                    response shouldHaveStatus HttpStatusCode.OK
+                    response.shouldHaveHeader(HttpHeaders.ETag, create.response.headers[HttpHeaders.ETag]!!)
+
+                    response exclusivelyHasTriples {
+                        validateRepoTriples(response, repoId, repoName, orgPath)
+                    }
+                }
+            }
+        }
     }
 }
