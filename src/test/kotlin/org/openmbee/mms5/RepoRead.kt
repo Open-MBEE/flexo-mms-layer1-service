a06ed5e15a8dbada757b86588230de7739112fc1
Jason Han <jason.han@jpl.nasa.gov>
2022-07-05 16:11:00 -0700

    Revert "WIP: Adding repo tests"
    This reverts commit 9021ed6909774cbc1c4ce54f8f20c935bf58f765.

--- src/test/kotlin/org/openmbee/mms5/RepoRead.kt
+++ src/test/kotlin/org/openmbee/mms5/RepoRead.kt
@@ -1,6 +1,5 @@
 package org.openmbee.mms5
 
-import io.kotest.assertions.ktor.shouldHaveHeader
 import io.kotest.assertions.ktor.shouldHaveStatus
 import io.kotest.core.test.TestCase
 import io.kotest.matchers.string.shouldNotBeBlank
@@ -12,46 +11,6 @@ import org.slf4j.LoggerFactory
 
 class RepoRead : RepoAny() {
     init {
-        "head non-existent repo" {
-            withTest {
-                httpHead(repoPath) {}.apply {
-                    response shouldHaveStatus HttpStatusCode.NotFound
-                }
-            }
-        }
-
-        "get non-existent repo" {
-            withTest {
-                httpGet(repoPath) {}.apply {
-                    response shouldHaveStatus HttpStatusCode.NotFound
-                }
-            }
-        }
-
-        "head repo" {
-            val create = createRepo(repoId, repoName, orgId)
-
-            withTest {
-                httpHead(repoPath) {}.apply {
-                    response shouldHaveStatus HttpStatusCode.OK
-                    response.shouldHaveHeader(HttpHeaders.ETag, create.response.headers[HttpHeaders.ETag]!!)
-                }
-            }
-        }
-
-        "get repo" {
-            val create = createRepo(repoId, repoName, orgId)
-
-            withTest {
-                httpGet(repoPath) {}.apply {
-                    response shouldHaveStatus HttpStatusCode.OK
-                    response.shouldHaveHeader(HttpHeaders.ETag, create.response.headers[HttpHeaders.ETag]!!)
-
-                    response exclusivelyHasTriples {
-                        validateRepoTriples(response, repoId, repoName, orgPath)
-                    }
-                }
-            }
-        }
+        //  tests...
     }
 }
