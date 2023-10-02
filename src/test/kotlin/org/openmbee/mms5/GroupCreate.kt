7a049c49431a63e2548c4d1c2b002c3821a2c4b2
Blake Regalia <blake.regalia@gmail.com>
2023-10-02 15:26:32 -0700

    test: group create

---
+++ src/test/kotlin/org/openmbee/mms5/GroupCreate.kt
@@ -0,0 +1,106 @@
+package org.openmbee.mms5
+
+import io.kotest.matchers.string.shouldNotBeBlank
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import org.apache.jena.vocabulary.DCTerms
+import org.apache.jena.vocabulary.RDF
+import org.openmbee.mms5.util.*
+import org.slf4j.LoggerFactory
+import java.net.URLEncoder
+
+fun TriplesAsserter.validateGroupTriples(
+    createResponse: TestApplicationResponse,
+    groupId: String,
+    extraPatterns: List<PairPattern> = listOf()
+) {
+    val groupIri = localIri("/groups/$groupId")
+
+    // org triples
+    subject(groupIri) {
+        exclusivelyHas(
+            RDF.type exactly MMS.Group,
+            MMS.id exactly groupId,
+            MMS.etag startsWith "",
+            *extraPatterns.toTypedArray()
+        )
+    }
+}
+
+fun TriplesAsserter.validateCreatedGroupTriples(
+    createResponse: TestApplicationResponse,
+    groupId: String,
+    extraPatterns: List<PairPattern> = listOf()
+) {
+    validateGroupTriples(createResponse, groupId, extraPatterns)
+
+    // transaction
+    validateTransaction()
+
+    // inspect
+    subject("urn:mms:inspect") { ignoreAll() }
+}
+
+
+class GroupCreate : CommonSpec() {
+    open val logger = LoggerFactory.getLogger(PolicyCreate::class.java)
+
+    val groupId = "ldap/cn=all.personnel,ou=personnel"
+    val groupPath = "/groups/${URLEncoder.encode(groupId, "UTF-8")}"
+
+    val groupTitle = "Test Group"
+    val validGroupBody = """
+        <>
+            dct:title "${groupTitle}"@en ;
+            .
+    """.trimIndent()
+
+    init {
+        "reject invalid group id".config(tags=setOf(NoAuth)) {
+            withTest {
+                httpPut("$groupPath/with/invalid/id") {
+                    setTurtleBody(validGroupBody)
+                }.apply {
+                    response shouldHaveStatus HttpStatusCode.BadRequest
+                }
+            }
+        }
+
+        mapOf(
+            "rdf:type" to "mms:NotGroup",
+            "mms:id" to "\"not-$groupId\"",
+        ).forEach { (pred, obj) ->
+            "reject wrong $pred".config(tags=setOf(NoAuth)) {
+                withTest {
+                    httpPut(groupPath) {
+                        setTurtleBody("""
+                            $validGroupBody
+                            <> $pred $obj .
+                        """.trimIndent())
+                    }.apply {
+                        response shouldHaveStatus HttpStatusCode.BadRequest
+                    }
+                }
+            }
+        }
+
+        "create valid group".config(tags=setOf(NoAuth)) {
+            withTest {
+                httpPut(groupPath) {
+                    setTurtleBody(validGroupBody)
+                }.apply {
+                    response shouldHaveStatus HttpStatusCode.OK
+                    response.headers[HttpHeaders.ETag].shouldNotBeBlank()
+
+                    response exclusivelyHasTriples {
+                        modelName = it
+
+                        validateCreatedGroupTriples(response, groupId, listOf(
+                            DCTerms.title exactly groupTitle
+                        ))
+                    }
+                }
+            }
+        }
+    }
+}
