7a41d1a0da936cf40b199d0f902fa7f4c9edc479
Blake Regalia <blake.regalia@gmail.com>
2023-11-06 21:35:03 -0800

    test: fix content-type assertion

--- src/test/kotlin/org/openmbee/mms5/RepoQuery.kt
+++ src/test/kotlin/org/openmbee/mms5/RepoQuery.kt
@@ -3,6 +3,7 @@ package org.openmbee.mms5
 import io.kotest.assertions.json.shouldBeJsonObject
 import io.kotest.assertions.json.shouldContainJsonKey
 import io.kotest.assertions.json.shouldContainJsonKeyValue
+import io.kotest.matchers.string.shouldStartWith
 import io.ktor.http.*
 import org.openmbee.mms5.util.*
 
@@ -28,7 +29,7 @@ class RepoQuery : ModelAny() {
                     setSparqlQueryBody(withAllTestPrefixes(lockCommitQuery))
                 }.apply {
                     response shouldHaveStatus HttpStatusCode.OK
-                    response.shouldHaveHeader("Content-Type", "application/sparql-results+json; charset=UTF-8")
+                    response.headers["Content-Type"] shouldStartWith "application/sparql-results+json"
                     response.content!!.shouldBeJsonObject()
                     response.content!!.shouldContainJsonKeyValue("$.results.bindings[0].etag.value", etag)
                     response.content!!.shouldContainJsonKey("$.results.bindings[0].time.value")
