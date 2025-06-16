24bc7cca5de60ca3e1505a3532e566d62a23d15b
Doris Lam <dlamoris@gmail.com>
2025-06-16 14:18:57 -0700

    revert ktor version change, change results in tests taking 2 hours to run

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
     testImplementation("org.apache.jena:jena-rdfconnection:${jenaVersion}");
     testFuseki("org.apache.jena:jena-fuseki-server:$jenaVersion")
 
-    val ktorVersion = "2.3.13"
+    val ktorVersion = "2.3.4"
     implementation("io.ktor:ktor-client-core:${ktorVersion}")
     implementation("io.ktor:ktor-client-content-negotiation:${ktorVersion}")
     implementation("io.ktor:ktor-client-cio:${ktorVersion}")
