396eae9d68a6dad551df2bf0b3f8f282ebd7d2a6
Doris Lam <dlamoris@gmail.com>
2026-04-18 15:59:35 -0700

    move vars

--- src/test/kotlin/org/openmbee/flexo/mms/CollectionAny.kt
+++ src/test/kotlin/org/openmbee/flexo/mms/CollectionAny.kt
@@ -14,20 +14,92 @@ open class CollectionAny : RepoAny() {
     val demoCollectionName = "Test Collection"
     val demoCollectionPath = "$basePathCollections/$demoCollectionId"
 
-    val fooCollectionId = "foo-collection"
-    val fooCollectionName = "Foo Collection"
-    val fooCollectionPath = "$demoOrgPath/collections/$fooCollectionId"
-
-    val barCollectionId = "bar-collection"
-    val barCollectionName = "Bar Collection"
-    val barCollectionPath = "$demoOrgPath/collections/$barCollectionId"
-
     // collections need a repo with a branch to collect
     val demoBranchRef = "$demoRepoPath/branches/master"
 
     val validCollectionBody = """
         <> dct:title "$demoCollectionName"@en .
         <> mms:collects <$demoBranchRef> .
+    """.trimIndent()
+
+    val masterBranchPath = "$demoRepoPath/branches/master"
+    // a second repo for cross-repo collection tests
+    val secondRepoId = "second-repo"
+    val secondRepoName = "Second Repo"
+    val secondRepoPath = "$demoOrgPath/repos/$secondRepoId"
+    val secondMasterBranchPath = "$secondRepoPath/branches/master"
+    val secondLockId = "lock-1"
+    val secondLockPath = "$secondRepoPath/locks/$secondLockId"
+
+    val collectionLockId = "demo-lock"
+    val collectionLockPath = "$demoRepoPath/locks/$collectionLockId"
+
+    val scratchId = "scratch-1"
+    val scratchPath = "$demoRepoPath/scratches/$scratchId"
+
+    // insert Alice into first repo's master branch
+    val insertAlice = """
+        $demoPrefixesStr
+
+        insert data {
+            :Alice a :Person ;
+                foaf:name "Alice" ;
+                .
+        }
+    """.trimIndent()
+
+    // insert Bob into second repo's master branch
+    val insertBob = """
+        $demoPrefixesStr
+
+        insert data {
+            :Bob a :Person ;
+                foaf:name "Bob" ;
+                .
+        }
+    """.trimIndent()
+
+    // insert Charlie into scratch
+    val insertCharlie = """
+        $demoPrefixesStr
+
+        insert data {
+            :Charlie a :Person ;
+                foaf:name "Charlie" ;
+                .
+        }
+    """.trimIndent()
+
+    // query to select all person names, ordered
+    val queryPersonNames = """
+        $demoPrefixesStr
+
+        select ?name where {
+            ?s a :Person .
+            ?s foaf:name ?name .
+        } order by asc(?name)
+    """.trimIndent()
+
+    // ASK query for Alice
+    val askAlice = """
+        $demoPrefixesStr
+
+        ask {
+            :Alice a :Person .
+        }
+    """.trimIndent()
+
+    // CONSTRUCT query for all persons
+    val constructPersons = """
+        $demoPrefixesStr
+
+        construct {
+            ?s a :Person ;
+                foaf:name ?name .
+        } where {
+            ?s a :Person ;
+                foaf:name ?name .
+        }
     """.trimIndent()
 
     override suspend fun beforeEach(testCase: TestCase) {
